badeaf49259eccf6a180148c8483c31737731de1
ghwns93 <150601337+ghwns93@users.noreply.github.com>
2024-01-25 13:54:27 +0900

    미니맵 제작중
    미니맵 제작중

--- UnityNewPortfolio/Assets/Script/GameManager.cs
+++ UnityNewPortfolio/Assets/Script/GameManager.cs
@@ -6,7 +6,10 @@ public class GameManager : MonoBehaviour
 {
     GameObject startPos;
     GameObject player;
+    GameObject playerBody;
+    GameObject playerPos;
 
+    public GameObject map;
     public GameObject playerPrefeb;
 
     // Start is called before the first frame update
@@ -17,7 +20,8 @@ public class GameManager : MonoBehaviour
 
         if(player == null )
         {
-            player = Instantiate(playerPrefeb) as GameObject;
+            player = Instantiate(playerPrefeb);
+            playerBody = player.transform.Find("PlayerBody").gameObject;
         }
 
         DontDestroyOnLoad(player);
@@ -27,5 +31,28 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         player.transform.position = startPos.transform.position;
+
+        if (map != null)
+        {
+            map.SetActive(false);
+            playerPos = map.transform.Find("PlayerPos").gameObject;
+        }
+    }
+
+    private void Update()
+    {
+        if(map != null)
+        {
+            if(Input.GetKeyDown(KeyCode.M))
+            {
+                map.SetActive(!map.activeSelf);
+            }
+
+            playerBody.SetActive(!map.activeSelf);
+
+            playerPos.transform.position = playerBody.transform.position;
+            playerPos.transform.position += new Vector3(0, 100, 0);
+
+        }
     }
 }
