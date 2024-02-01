07e2f3ba251dc3c593a76424bb52b39e8b93f0e5
eweatew <apo3221@gmail.com>
2024-02-01 23:06:40 +0900

    타이틀

---
+++ UnityNewPortfolio/Assets/Script/SoundManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public Slider volumeSlider;
+
+    void Start()
+    {
+        float savedVolume = PlayerPrefs.GetFloat("SoundVolume", 1.0f);
+        SetVolume(savedVolume);
+
+        volumeSlider.value = savedVolume;
+        volumeSlider.onValueChanged.AddListener(ChangeVolume);
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        SetVolume(volume);
+    }
+
+    void SetVolume(float volume)
+    {
+        AudioListener.volume = Mathf.Clamp01(volume);
+        PlayerPrefs.SetFloat("SoundVolume", AudioListener.volume);
+    }
+}
